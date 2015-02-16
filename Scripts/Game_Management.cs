364f32152be6c2c4c413e646786aafb448131fca
Clarence Chow <clarencegod@gmail.com>
2015-02-16 18:12:25 +0800

    first commit

---
+++ Scripts/Game_Management.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Management : MonoBehaviour {
+	public GameObject grid;
+	public GameObject tile;
+	public GameObject[] ObjList;
+	public int[,] Area;
+	public int gridX, gridY;
+	public int currentTileAmount = 0;
+
+	private enum game_State {
+		invalid_state = -1,
+		Loaded,
+		WaitForPlayer,
+		Checking,
+		GameOver
+	}
+
+	void Awake () {
+		Area = new int[gridX, gridY];
+		for (int x = 0; x < gridX; x++) {
+			for (int y = 0; y < gridY; y++) {
+				GameObject go = Instantiate(grid, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+				// go.transform.parent = this.transform;
+			}
+		}
+		//gameObject.transform.position = new Vector3(0f, 0f, 0f);
+	}
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+
+	private static Vector2 GridToWorldPoint(int x, int y){
+		return new Vector2 (x, y);
+	}
+
+	private static Vector2 WorldToGridPoint(float x, float y){
+		return new Vector2 (x, y);
+	}
+
+	void AddRandomTile(){
+		if (currentTileAmount >= (gridX * gridY)) {
+			//throw new UnityException("Game Over");
+			currentTileAmount = 0;
+			Object[] Tiles = GameObject.FindGameObjectsWithTag("Tile");
+			foreach( Object Tile in Tiles){
+				Destroy(Tile);
+			}
+		}
+		int id = Random.Range (1, 4);
+		int x = Random.Range (0, gridX);
+		int y = Random.Range (0, gridY);
+		bool exist = false; // boolean checker to see if there is already a tile
+		while (!exist) {
+			if(Area[x,y] == 0){ // if there is no tile 
+				exist = true;
+				Area[x,y] = id;
+				Vector2 worldposition = GridToWorldPoint(x,y);
+				GameObject obj;
+				obj = Instantiate(tile, worldposition, transform.rotation) as GameObject;
+				currentTileAmount++; 
+			}
+		x++; // If there is a tile then move to next tile
+			// Move on to the next avaliable tile
+			if (x >= gridX){
+				y++;
+				x = 0;
+			}
+			if ( y >= gridY){
+				y = 0;
+			}
+		}
+	}
+
+	void MoveToUp(){
+
+	}
+
+	void MoveToDown(){
+		
+	}
+
+	void MoveToLeft(){
+		for (int x = 1; x < gridX; x++) {
+			for (int y = gridY - 1; y >= 0; y--){
+				if (Area[x, y] == 0){
+					continue;
+				}
+
+			}
+		}
+	}
+
+	void MoveToRight(){
+		
+	}
+}
---
+++ Scripts/SwipeDetection.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeDetection : MonoBehaviour {
+	private Vector2 initialPos;
+	private Vector2 posDiff;
+
+	void Update() {
+		if (Input.GetMouseButtonDown (0)) {
+			//print("Touchbegan!");
+			initialPos = Input.mousePosition;
+		}
+		
+		if (Input.GetMouseButtonUp (0)) {
+			posDiff = (Vector2)Input.mousePosition - initialPos;
+			print(posDiff);
+			if (posDiff.y > 225){
+				print("UP");
+				SendMessage("AddRandomTile");
+			} else if (posDiff.y < -225){
+				print ("DOWN");
+				SendMessage("AddRandomTile");
+			} else if (posDiff.x > 225){
+				print("RIGHT");
+				SendMessage("AddRandomTile");
+			} else if (posDiff.x < -225){
+				print("LEFT");
+				SendMessage("AddRandomTile");
+			}
+		}
+	}
+//		void Update () {
+//		if (Input.touchCount > 0) {
+//			print("touching");
+//			Touch touch = Input.GetMouseButtonDown(0);
+//
+//			switch (touch.phase){
+//
+//			case TouchPhase.Began:
+//				initialPos = touch.position;
+//				break;
+//
+//			case TouchPhase.Ended:
+//				float swipeDistVert = (new Vector3(0, touch.position.y, 0) - new Vector3(0, initialPos.y, 0)).magnitude;
+//				if (swipeDistVert > minSwipeDistY){
+//					float swipeValue = Mathf.Sign(touch.position.y - initialPos.y);
+//					if (swipeValue > 0){
+//						Debug.Log("UP");
+//					} else if (swipeValue < 0){
+//						Debug.Log("Down");
+//					}
+//				}
+//				break;
+//			}
+//		}
+//	}
+
+}
